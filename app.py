7ca0583e6564857296c688985b112e0c86d3037a
Arnav Verma <arnavv25@gmail.com>
2025-09-03 23:35:37 -0700

    Added new route for adding  names

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def helloworld():
     return render_template("hello.html")
+
+@app.route('/<name>')
+def helloname(name):
+    return f"<p>hello, {name}!</p>"
